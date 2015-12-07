60f45db7fc51a56e0d8bc38516240de27b99449b
Zach Arnold <me@zacharnold.org>
2015-12-06 22:14:36 -0800

    adds prayer request, testimony, and user models/controllers to the app

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+
+  resources :prayer_requests
+  resources :testimonies
+  resources :users
+
   root 'welcome#index'
 
   # Example of regular route:
