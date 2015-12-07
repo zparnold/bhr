8fea42ff6fe439e689695c6191f2cdc78265d4c1
Zach Arnold <me@zacharnold.org>
2015-12-07 07:27:12 -0800

    forgot to migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151207055341) do
+ActiveRecord::Schema.define(version: 20151207142254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,22 @@ ActiveRecord::Schema.define(version: 20151207055341) do
     t.text     "prayer_request"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "when"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "super_users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "username"
+    t.string   "password_hash"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "testimonies", force: :cascade do |t|
